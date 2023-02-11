461e5cb9c76e3cb50ff3c1ea2805df40bbc82817
yndgow <yndgow@gmail.com>
2023-02-12 03:19:35 +0900

    my ordered

--- Kmarket2/js/my/home.js
+++ Kmarket2/js/my/home.js
@@ -1,7 +1,13 @@
 document.addEventListener('DOMContentLoaded',()=>{
+    qnaStat();
+    asideAddClass();
+});
+
+
+// 문의내역 - 상태 변경(0 = 검토중, 1 = 답변완료) 
+function qnaStat(){
     let answer = document.getElementsByClassName('qna_stat');
     for (let i = 0; i < answer.length; i++) {
-
         if(answer[i].textContent === '0'){
             answer[i].textContent = '검토중';
             answer[i].style.color = 'gray';
@@ -9,9 +15,24 @@ document.addEventListener('DOMContentLoaded',()=>{
             answer[i].textContent = '답변완료';
             answer[i].style.color = 'green';
         }
-        console.log(answer[i]);
     }
-    
-    
+}
 
-});
+// 
+function asideAddClass(){
+    
+    let cur_url = window.location.href;
+    let ext = cur_url.lastIndexOf("."); // . 의 마지막 인덱스 가져오기
+    let last_index = cur_url.lastIndexOf("/"); // /의 마지막 인덱스 가져오기
+    let str = cur_url.substring(last_index+1, ext); // 현재 주소에서 확장자를 뺀 마지막 경로 이름 가져오기 ex) home
+
+    let asideOn = document.getElementsByClassName("aside_on");
+    let asideNameList = ["ordered", "point", "coupon", "review", "qna", "info"];
+
+    // str의 
+    for(let i = 0; i<asideNameList.length; i++){
+        if(str === asideNameList[i]){
+            asideOn[i].setAttribute("class", "on");
+        }    
+    }
+}
